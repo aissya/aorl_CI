c3cd13544b6aa403ee0c01db8d3800793f457be6
DIAH AYU NUR HIDAYATI <99608576+Diahayunurhidayati@users.noreply.github.com>
2022-08-10 08:59:54 +0700

    halam blocked acces

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -3,6 +3,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class User extends CI_Controller 
 {
+    public function __construct()
+    {
+        parent::__construct();
+        is_logged_in();
+    }
+    
     public function index()
     {
         //mengambil data dari session usrdata pada Auth berdasarkan email yang ada pada session 
